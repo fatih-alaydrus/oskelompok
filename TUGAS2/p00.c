ad6d4b6c91939d6fe1762517e8b23b46206094e0
reynardryanda <reynardryanda@gmail.com>
2020-06-24 09:17:17 +0700

    OS201 reynardryanda

--- TUGAS2/p00.c
+++ TUGAS2/p00.c
@@ -8,8 +8,9 @@
 #define  akunGitHub "stefansagala"
 #include "p00.h"
 
-char*    progs[]={P01, P02, P03};
+char*    progs[]={P01, P02, P03, P04, P05, P06};
 myshare* mymap;
+int      entry;
 
 int init(void) {
     int ssize=sizeof(myshare);
@@ -18,37 +19,66 @@ int init(void) {
     ftruncate(fd, ssize);
     mymap=mmap(NULL, ssize, MYPROTECTION, MYVISIBILITY, fd, 0);
     close(fd);
-    //  more INIT STUFFs...
-    //  more INIT STUFFs...
-    //  more INIT STUFFs...
+    
+    sem_init(&mymap->mutex, 1, 1);
+    mymap->state = OPEN;
+
     return BOSS;
 }
 
-char tmpStr[256]={};  // a temporary string.
+char tmpStr[256]={};  // temp string
 void myprint(char* str1, char* str2) {
-    printf("%s[%s]\n", str1, str2);
-    // blah blah blah
-    // blah blah blah
-    // blah blah blah
+    printf("%-14s[%s]\n", str1, str2);
 }
 
 int getEntry(void) {
-    int entry;
-    // get an entry number
     return entry;
 }
 
+// display MMAP entry
 void display(int entry) {
-    // display an entry of MMAP. Eg.
-    // akunGH2[progs[03] TIME[18] MUTEX[05] MMAP[OPEN] [akunGH1][akunGH3][akunGH0][akunGH2]]
+    int i;
+    char akun[256] = {};
+    int k = 0;
+    while( k < getEntry()){
+        sprintf(akun, "%s[%s]", akun, mymap->progs[k].akun);
+        k++;
+    }
+
+    char state[5] = {}; 
+    if (mymap->state == 255) 
+        sprintf(state, "OPEN");
+
+    sprintf(tmpStr, "progs[%2.2d] TIME[%2.2d] MUTEX[%2.2d] MMAP[%s] %s",
+        getEntry(), mymap->mutexctr, mymap->progs[getEntry()].stamp, state, akun);
+    myprint(akunGitHub, tmpStr);
 }
 
 void putInfo(char* akun, int entry) {
-    // put "akunGitHub" into akun[] array (MMAP)
+    strcpy(mymap->progs[entry].akun, akun);
 }
 
 void checkOpen(void) {
-    // exit if MMAP is closed.
+    if (mymap->state == CLOSED) {
+      printf("CLOSED, BYE BYE ==== ====");
+      exit(0);
+    }
+}
+
+void incrementBos(int x) {
+    sem_wait(&mymap->mutex);
+    if (x == 1){
+	entry = mymap->entry++;
+    }
+    mymap->mutexctr++;
+    mymap->progs[getEntry()].stamp++;
+    if (x == 2){
+        display(mymap->entry);
+    }
+    if (x == 3){
+        putInfo(akunGitHub, getEntry());
+    }
+    sem_post(&mymap->mutex);
 }
 
 int main(void) {
@@ -57,9 +87,33 @@ int main(void) {
     int boss=init();
     checkOpen();
     sleep (DELAY);
-    // blah... blah... blah...
-    // blah... blah... blah...
-    // blah... blah... blah...
-    myprint(akunGitHub, "BYEBYE =====  ===== =====");
-}
 
+    int pid = getpid();
+    int i = 0
+    while( i < sizeof(progs)/sizeof(char*)){
+        if (fork() == 0) {
+            execlp(progs[i], progs[i], NULL);
+        };
+        i++;
+    }
+
+    sleep (DELAY);
+
+    incrementBos(1);
+
+    incrementBos(2);
+
+    sleep (DELAY);
+
+    incrementBos(3);
+
+    int j = 0;
+    while( j < sizeof(progs)/sizeof(char*) ){
+        incrementBos(2);
+        wait(NULL);
+        j++;
+    }
+    
+    myprint(akunGitHub, "BYEBYE =====  ===== =====");
+    mymap->state = CLOSED;
+}
--- TUGAS2/p04.c
+++ TUGAS2/p04.c
@@ -8,51 +8,108 @@
 #define  akunGitHub "fatih-alaydrus"
 #include "p00.h"
 
-char*    progs[]={P01, P02, P03};
+char*    progs[]={P01, P02, P03, P04, P05, P06};
 myshare* mymap;
+int      entry;
 
 int init(void) {
     sleep(DELAY);
-    // blah blah blah
+
+    // exit when file is not initiated by parent (p00)
+    struct  stat fsize;
+    int     ssize = sizeof(myshare);
+    int     fd = open(SHAREMEM, MYFLAGS, CHMOD);
+    fstat(fd, &fsize);
+    if (fsize.st_size < ssize) {
+        printf("No \"SharedMemoryFile.bin\" file.\n");
+        exit(1);
+    }
+
+    fchmod(fd, CHMOD);
+    ftruncate(fd, ssize);
+    mymap=mmap(NULL, ssize, MYPROTECTION, MYVISIBILITY, fd, 0);
+    close(fd);
     return NOTBOSS;
 }
 
-char tmpStr[256]={};  // a temporary string.
+char tmpStr[256]={};
 void myprint(char* str1, char* str2) {
-    printf("%s[%s]\n", str1, str2);
-    // blah blah blah
-    // blah blah blah
-    // blah blah blah
+    printf("%-14s[%s]\n", str1, str2);
 }
 
+// get entry number
 int getEntry(void) {
-    int entry;
-    // get an entry number
     return entry;
 }
 
+// display an entry of MMAP
 void display(int entry) {
-    // display an entry of MMAP.
-    // eg. akunGH2[progs[03] TIME[18] MUTEX[05] MMAP[OPEN] [akunGH1][akunGH3][akunGH0][akunGH2]]
+    int i;
+    char akun[256] = {};
+    int k = 0;
+    while( k < getEntry()){
+        sprintf(akun, "%s[%s]", akun, mymap->progs[k].akun);
+        k++;
+    }
+
+    char state[5] = {}; 
+    if (mymap->state == 255) 
+        sprintf(state, "OPEN");
+
+    sprintf(tmpStr, "progs[%2.2d] TIME[%2.2d] MUTEX[%2.2d] MMAP[%s] %s",
+        getEntry(), mymap->mutexctr, mymap->progs[getEntry()].stamp, state, akun);
+    myprint(akunGitHub, tmpStr);
 }
 
 void putInfo(char* akun, int entry) {
-    // put "akunGitHub" into akun[] array (MMAP)
+    strcpy(mymap->progs[entry].akun, akun);
 }
 
+// exit if MMAP is closed
 void checkOpen(void) {
-    // exit if MMAP is closed.
+    if (mymap->state == CLOSED) {
+        printf("CLOSED, BYE BYE ==== ====\n");
+        exit(1);
+    }
 }
+
+void incrementBos(int x) {
+    sem_wait(&mymap->mutex);
+    if (x == 1){
+	entry = mymap->entry++;
+    }
+    mymap->mutexctr++;
+    mymap->progs[getEntry()].stamp++;
+    if (x == 2){
+        display(mymap->entry);
+    }
+    if (x == 3){
+        putInfo(akunGitHub, getEntry());
+    }
+    sem_post(&mymap->mutex);
+}
+
 
 int main(void) {
     sprintf(tmpStr, "START PID[%d] PPID[%d]", getpid(), getppid());
     myprint(akunGitHub, tmpStr);
     int boss=init();
     checkOpen();
+
     sleep (DELAY);
-    // blah... blah... blah...
-    // blah... blah... blah...
-    // blah... blah... blah...
+    
+    incrementBos(1);
+    
+    incrementBos(2);
+
+    sleep (DELAY);
+
+    incrementBos(3);
+
+    sleep(DELAY);
+
+    incrementBos(2);
+    incrementBos(2);
+
     myprint(akunGitHub, "BYEBYE =====  ===== =====");
 }
-
