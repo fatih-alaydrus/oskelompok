fc3ce707a5efa09a5f5c70c4261b11f25aedcaa6
fatih-alaydrus <45792191+fatih-alaydrus@users.noreply.github.com>
2020-07-03 11:03:44 +0700

    Update p05.c

--- TUGAS3/p05.c
+++ TUGAS3/p05.c
@@ -1,5 +1,13 @@
 /*
 // Tambahkan di sini nama para kontributor (akunGitHub)
+Kontributor: 
+abhiprayatj
+stefansagala
+reynardryanda
+fatih-alaydrus
+raniapriliaa
+billhsyn
+fadhilhmr
 // Harap UPDATE tanggal revisi!
 // Tue Jun  9 17:46:47 WIB 2020
  */
